d6124f7b8208140f0d080f251e70ad1dcf5d3173
Kevin Kuchta <kevin@kevinkuchta.com>
2021-08-30 13:09:04 -0700

    Initial revision - raw bundle gem

---
+++ spec/wacktrace_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Wacktrace do
+  it "has a version number" do
+    expect(Wacktrace::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
